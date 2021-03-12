759025d18b627579aa2130c1d8dbd92314e0402c
Karanraj <karanrajmokan@gmail.com>
2021-03-12 21:34:13 +0530

    Adding login module to frontend

--- Leave Planner Frontend/src/App.js
+++ Leave Planner Frontend/src/App.js
@@ -1,11 +1,21 @@
 import { Component } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import logo from './images/LP-logo.png';
+import Login from './login';
 
 class App extends Component {
 
   render() {
-    return (<p></p>);
+      return (
+      <div>
+        <img className="image-div" src={logo} alt=""></img>
+        <div className="line-div">
+          <div className="inner-div">
+              <Login/>
+          </div>
+        </div>
+      </div>);
   }
 
 }
---
+++ Leave Planner Frontend/src/login.js
@@ -0,0 +1,67 @@
+import { Component } from 'react';
+import './login.css';
+import axios from 'axios';
+
+class Login extends Component{
+
+    logIn(cred)
+    {
+        axios.request({
+            method:'post',
+            url:'http://localhost:8080/login-details',
+            data: cred
+        }).then(response=>{
+            console.log(response.data);
+            this.props.history.push({
+                pathname:"/homepage",
+                state:{ detail:response.data }
+            });
+        }).catch(error => {
+            console.log(error.response)
+        });
+    }
+
+
+    onSubmit(e)
+    {
+        const List = {
+            rollno: this.refs.rollno.value,
+            password: this.refs.password.value
+        }
+        console.log(List);
+        //this.logIn(List);
+        e.preventDefault();
+    }
+
+    render(){
+        return(
+            <div>
+                <div className="login-component">
+                    <p className="title-text"> Welcome to Leave Planner </p>
+                    <div className="lines"></div>
+                    <p className="fonts position1">Roll number</p>
+                    <p className="fonts position2">Password</p>
+                    <form onSubmit={this.onSubmit.bind(this)}>  
+                        <div className="pad">
+                            <div className="box center">
+                                <input type="text" className="form-control" placeholder="Enter rollno" ref="rollno"/>
+                            </div>
+                        </div>
+
+                        <div className="pad">
+                            <div className="box center">
+                                <input type="password" className="form-control" placeholder="Enter password" ref="password"/>
+                            </div>
+                        </div>
+
+                        <div className="pad-but">
+                            <button type="submit" className="submit center"><p className="but-text">Login</p></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login;
